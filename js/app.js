123986895a9a8d0742f08024dd15b093b19cd063
Lange Camille <21603426@etu.unicaen.fr>
2018-02-17 18:44:53 +0100

    Ajout d'un peu de CSS

--- js/app.js
+++ js/app.js
@@ -1,3 +1,8 @@
+Vue.component('ligne', {
+  props: ['id','name','created_at','description','clone_url'],
+  template: '<tr><td>{{ id }}</td><td>{{ name }}</td><td>{{ created_at }}</td><td>{{ description }}</td><td>{{ clone_url }}</td></tr>'
+})
+
 var agent = new Vue({
   el: '#agent',
   data: {
@@ -19,10 +24,23 @@ var agent = new Vue({
     getData: function(){
       axios.get("https://api.github.com/users/"+this.nom+"/repos")
         .then((response) => {
-          console.log(response.data);
+
+          var tableau = document.getElementById('tableau')
+
+          if (tableau.style.display == "")
+              tableau.style.display = "inline-block"
+
           for(var i = 0; i < response.data.length; i ++ )
           {
-            console.log("repos name : " + response.data[i].name)
+            document.getElementById('corps').innerHTML += '' +
+            '<tr class=" color' + i%2 + '">' +
+              '<td>' + response.data[i].id          + '</td>' +
+              '<td>' + response.data[i].name        + '</td>' +
+              '<td>' + response.data[i].created_at  + '</td>' +
+              '<td>' + response.data[i].description + '</td>' +
+              '<td>' + response.data[i].clone_url   + '</td>' +
+            '</tr>'
+            // '<ligne id="' +response.data[i].id+ '" name="' +response.data[i].name+ '" created_at="' +response.data[i].created_at+ '" description="' +response.data[i].description+ '" clone_url="' +response.data[i].clone_url+ '"></ligne>'
           }
         })
     }
