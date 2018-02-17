a2f7b6184e543d8d656193f3eeba8c021dcf5601
Lange Camille <21603426@etu.unicaen.fr>
2018-02-17 17:02:01 +0100

    récupération de l'id d'un utilisateur - réussit

--- js/app.js
+++ js/app.js
@@ -9,7 +9,17 @@ var agent = new Vue({
       if(this.nom == '')
         this.texte = 'J\'attend le nom de l\'agent'
       else
+      {
         this.texte = "le nom de votre agent est : " + this.nom
+        this.getData();
+      }
+    }
+  },
+  methods: {
+    getData: function(){
+      axios.get("https://api.github.com/users/"+this.nom).then((response) => {
+        console.log(response.data.id)
+      })
     }
   }
 })
