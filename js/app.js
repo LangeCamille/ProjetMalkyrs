7b57220596594072e80bf2c9b3a68ccd60a806f0
Lange Camille <21603426@etu.unicaen.fr>
2018-02-18 16:31:40 +0100

    implémentation du filtre actif

--- js/app.js
+++ js/app.js
@@ -4,32 +4,34 @@
 // })
 const store = new Vuex.Store({
   state: {
-    lignes: [
-      null
-    ],
+    lignes: [null],
     head:[
      "Id",
      "Nom",
      "Créé le",
+     "Mise à jour le",
      "Description",
      "Clone URL"
    ]
   },
   mutations: {
-    addLigne(state, ligne)
-    {
+    addLigne(state, ligne) {
       var newDate = ligne.date.split("T")
       newDate = newDate[0].split("-")
+
+      var newMaJ = ligne.mAj.split("T")
+      newMaJ = newMaJ[0].split("-")
+
       state.lignes[state.lignes.length] = {
         id: ligne.id,
         name: ligne.name,
-        date: newDate[2]+"/"+newDate[1]+"/"+newDate[0], // BUG: faire un traitement pour rendre la date potable
+        date: newDate[2]+"/"+newDate[1]+"/"+newDate[0],
+        mAj: newMaJ[2]+"/"+newMaJ[1]+"/"+newMaJ[0],
         desc: ligne.desc,
         url: ligne.url
       }
     },
-    viderTableau(state)
-    {
+    viderTableau(state) {
       state.lignes = []
     }
   }
@@ -41,7 +43,8 @@ const agent = new Vue({
     store,
     nom: '',
     cacher: true,
-    texte: 'J\'attend le nom de l\'agent'
+    texte: 'J\'attend le nom de l\'agent',
+    actifCheck: false
   },
   watch: {
     nom: function(newText, oldText) {
@@ -75,6 +78,7 @@ const agent = new Vue({
                             response.data[i].id,
                             response.data[i].name,
                             response.data[i].created_at,
+                            response.data[i].updated_at,
                             response.data[i].description,
                             response.data[i].clone_url)
               // '<ligne id="' +response.data[i].id+ '" name="' +response.data[i].name+ '" created_at="' +response.data[i].created_at+ '" description="' +response.data[i].description+ '" clone_url="' +response.data[i].clone_url+ '"></ligne>'
@@ -94,15 +98,53 @@ const agent = new Vue({
       },
       500
     ),
-    addLigne(newid, newname, created_at, description, clone_url){
+    addLigne(newid, newname, created_at, updated_at, description, clone_url){
       var ligne = {
         id: newid,
         name: newname,
         date: created_at,
+        mAj: updated_at,
         desc: description,
         url: clone_url
       }
       store.commit('addLigne',ligne)
     },
+    recent: function(){
+
+    },
+    actif: function(){
+      if (this.actifCheck)
+        this.actifCheck = false
+      else
+        this.actifCheck = true
+
+      var vue = this
+      var date = new Date();
+
+      var mois
+      if(date.getMonth() < 10)
+        mois = "0"+date.getMonth()
+      else
+        mois = date.getMonth()
+
+      for (var i = 1; i < store.state.lignes.length; i++) {
+        var ligne = document.getElementById(i)
+        if (vue.actifCheck)
+        {
+          ligne.classList.add("cacher")
+          axios.get("https://api.github.com/repos/"+this.nom+"/"+this.store.state.lignes[i].id+"/commits?since="+date.getFullYear()+"-"+mois+"-"+date.getDate()+"T00:00:00Z")
+          .then((response) =>
+          {
+            ligne.classList.remove("cacher")
+          })
+          .catch(function () {
+            console.log("Erreur ! Impossible d'accéder à l'API.")
+          })
+        }
+        else {
+          ligne.classList.remove("cacher")
+        }
+      }
+    }
   }
 })
